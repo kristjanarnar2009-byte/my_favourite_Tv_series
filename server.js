71c5756c14665266273082eab75ddf41d054189a
kristjan arnar <kristjanarnar2009@gmail.com>
2026-03-25 10:06:43 +0000

    25.march

--- server.js
+++ server.js
@@ -4,20 +4,14 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.set('views', path.join(__dirname, "scr", "views"));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, "views"));
 
-app.use('/', showRouter);
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use((req, res, next) => {
-    res.status(404).render('404', { title: 'Síða fannst ekki' });
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Eitthvað fór úrskeiðist');
+app.get('/', (req, res) => {
+    res.render('index', { title: 'My Little Pony vefurinn'});
 });
 
 app.listen(PORT, () => {
