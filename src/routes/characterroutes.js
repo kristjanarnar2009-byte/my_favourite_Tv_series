fd959734f7a7f6ee425b4e4f89c2684e7a338dad
kristjan arnar <kristjanarnar2009@gmail.com>
2026-04-30 10:36:01 +0000

    30.april

--- src/routes/characterroutes.js
+++ src/routes/characterroutes.js
@@ -4,4 +4,10 @@ const characterController = require('../controllers/characterController');
 
 router.get('/', characterController.getHomePage);
 
+router.get('/character/ny', characterController.getAddCharacterForm);
+
+router.post('/character/ny', characterController.createNewCharacter);
+
+router.get('/character/:id', characterController.getCharacterDetails);
+
 module.exports = router;
